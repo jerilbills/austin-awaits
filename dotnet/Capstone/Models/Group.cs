34fd3927e5fa0e074a825bf7f5dbb9937ed4aef4
Jeril Bills <jerilbills@gmail.com>
2023-12-01 19:04:45 -0500

    Add properties to Group

---
+++ dotnet/Capstone/Models/Group.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Data.Common;
+
+namespace Capstone.Models
+{
+    public class Group
+    {
+        public int Id { get; set; }
+        public int OwnerId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Avatar { get; set; }
+        public List<User> Users { get; set; }
+    }
+}
