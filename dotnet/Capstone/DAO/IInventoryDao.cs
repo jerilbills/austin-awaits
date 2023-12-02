58f74fb8ad8e13dab73dcb9c5a72d04afbdd4e44
Jeril Bills <jerilbills@gmail.com>
2023-12-02 16:46:37 -0500

    Add methods to IInventoryDao interface

--- dotnet/Capstone/DAO/IInventoryDao.cs
+++ dotnet/Capstone/DAO/IInventoryDao.cs
@@ -1,6 +1,12 @@
-﻿namespace Capstone.DAO
+﻿using Capstone.Models;
+
+namespace Capstone.DAO
 {
     public interface IInventoryDao
     {
+        public Inventory AddItem(Item itemToAdd);
+        public Inventory RemoveItem(Item itemToRemove);
+        public Inventory GetInventory();
+        public int GetInventoryItemQuantity(Item itemToCheck);
     }
 }
