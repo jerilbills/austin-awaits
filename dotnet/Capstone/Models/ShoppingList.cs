4cc901b6044603516d8a4c6ac9043aa670aca491
Jeril Bills <jerilbills@gmail.com>
2023-12-01 19:18:18 -0500

    Add properties to ShoppingList

---
+++ dotnet/Capstone/Models/ShoppingList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Capstone.Models
+{
+    public class ShoppingList
+    {
+        public int Id { get; set; }
+        public int OwnerId { get; set; }
+        public string Name { get; set; }
+        public int ClaimantId { get; set; }
+        public DateTime DueDate { get; set; }
+        public string Status { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime LastModified { get; set; }
+    }
+}
