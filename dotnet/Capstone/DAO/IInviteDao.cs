5f59409920cf78dfde07a2d5a993403d73eca3a0
Jeril Bills <jerilbills@gmail.com>
2023-12-02 18:17:32 -0500

    Add methods to IInviteDao and IItemDao

--- dotnet/Capstone/DAO/IInviteDao.cs
+++ dotnet/Capstone/DAO/IInviteDao.cs
@@ -1,9 +1,12 @@
 ﻿using Capstone.Models;
+using System.Collections.Generic;
 
 namespace Capstone.DAO
 {
     public interface IInviteDao
     {
         public Invite Invite(User userToInvite);
+        public Invite GetInviteById(int id);
+        public List<Invite> GetInvites();
     }
 }
--- dotnet/Capstone/DAO/IItemDao.cs
+++ dotnet/Capstone/DAO/IItemDao.cs
@@ -1,6 +1,13 @@
-﻿namespace Capstone.DAO
+﻿using Capstone.Models;
+using System.Collections.Generic;
+
+namespace Capstone.DAO
 {
     public interface IItemDao
     {
+        public Item CreateItem();
+        public Item UpdateItem(Item itemToUpdate);
+        public Item GetItemById(int id);
+        public List<Item> GetItems();
     }
 }
