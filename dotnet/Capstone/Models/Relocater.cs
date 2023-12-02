1bdbf091b6958885fba789c6c0a03b1ff9ac75d1
Jeril Bills <jerilbills@gmail.com>
2023-12-02 14:01:39 -0500

    Add a Relocater model

---
+++ dotnet/Capstone/Models/Relocater.cs
@@ -0,0 +1,10 @@
+﻿namespace Capstone.Models
+{
+    public class Relocater
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Department { get; set; }
+    }
+}
