29e5a2f8ea9e20a64c5a40c3ee26991bd752f6a9
Jeril Bills <jerilbills@gmail.com>
2023-12-04 19:32:00 -0500

    Add CreateGroupButton component to HomeView

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -2,10 +2,16 @@
   <div class="home">
     <h1>Home</h1>
     <p>You must be authenticated to see this</p>
+    <create-group-button></create-group-button>
   </div>
 </template>
 
 <script>
+import CreateGroupButton from '../components/CreateGroupButton.vue';
+
 export default {
+  components: {
+    CreateGroupButton
+  }
 };
 </script>
