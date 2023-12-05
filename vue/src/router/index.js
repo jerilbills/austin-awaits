3d0c4e044a4cd56899f30e5e97e92f68b24cece0
Dustin Miller <dustmil@icloud.com>
2023-12-05 14:26:38 -0500

    Created Navbar, Sidebar, Taskcard, and Kanban components. Created listview page

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -6,6 +6,7 @@ import HomeView from '../views/HomeView.vue';
 import LoginView from '../views/LoginView.vue';
 import LogoutView from '../views/LogoutView.vue';
 import RegisterView from '../views/RegisterView.vue';
+import PageVue from '../views/Page.vue';
 
 /**
  * The Vue Router is used to "direct" the browser to render a specific view component
@@ -19,7 +20,7 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView,
+    component: PageVue,
     meta: {
       requiresAuth: true
     }
