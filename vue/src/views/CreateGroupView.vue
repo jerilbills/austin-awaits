14570c174753318cbc5c7de4923788d67bcb3501
Jeril Bills <jerilbills@gmail.com>
2023-12-04 22:56:27 -0500

    Add SET_NOTIFICATION mutation to store, add sumbit button to form to create group

--- vue/src/views/CreateGroupView.vue
+++ vue/src/views/CreateGroupView.vue
@@ -4,6 +4,7 @@
         <input id="group-name" type="text">
         <label for="group-description">Description</label>
         <textarea name="group-description" id="group-description" cols="30" rows="10"></textarea>
+        <input type="submit">
     </form> 
 </template>
 
