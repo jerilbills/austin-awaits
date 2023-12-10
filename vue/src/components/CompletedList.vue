90800c26061571e1bff3e9bea93381f95639b84b
Jeril Bills <jerilbills@gmail.com>
2023-12-09 19:30:02 -0500

    Add a header to CompletedList component

--- vue/src/components/CompletedList.vue
+++ vue/src/components/CompletedList.vue
@@ -1,9 +1,29 @@
 <template>
-    <h1></h1>
+    <div class="kanban-board-header">
+        <div class="page-title">{{ "List Name Here" }}
+        </div>
+        <div class="invites">
+            <div class="is-size-7">List Owner</div>
+            <div><img src="https://api.dicebear.com/7.x/initials/svg?seed=JB" class="avatar"></div>
+            <div>&nbsp;&nbsp;</div>
+            <div class="is-size-7">List Members</div>
+            <div>
+                <img src="https://api.dicebear.com/7.x/initials/svg?seed=JB" class="avatar">
+                <img src="https://api.dicebear.com/7.x/initials/svg?seed=DM" class="avatar">
+                <img src="https://api.dicebear.com/7.x/initials/svg?seed=NH" class="avatar">
+                <img src="https://api.dicebear.com/7.x/initials/svg?seed=JF" class="avatar">
+            </div>
+            <div><i class="fa fa-user-plus fa-lg"></i></div>
+        </div>
+    </div>
 </template>
 
 <script>
-
+export default {
+computed: {
+    
+}
+}
 </script>
 
 <style>
