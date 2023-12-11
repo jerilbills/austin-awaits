7630d4cbb99bb0f2d6c6e50ff9a7692665a58a82
DustinMiller412 <dustmil@icloud.com>
2023-12-10 21:09:39 -0500

    created adminsidebar

--- vue/src/components/AdminSidebar.vue
+++ vue/src/components/AdminSidebar.vue
@@ -1,132 +1,138 @@
 <template>
     <div class="sidebar is-dark">
-        <span class="sidebar-header"><span class="icon"><i class="fa fa-home"></i></span>My Lists</span>
-        <div v-if="lists.length > 0">
+        <span class="sidebar-header"><span class="icon"><i class="fa fa-home"></i></span> Active Lists</span>
+        <div v-for="(department, index) in departments" :key="index">
+            <p><em>{{ department.departmentName }}</em></p>
             <ul>
-                <li v-for="list in inProgressLists" :key="list.listId"
-                    @click="navigateTo(this.$store.state.user.departmentId, list.listId)">
+                <li v-for="list in inProgressListsByDepartment(department.departmentName)" :key="list.listId"
+                    @click="navigateTo(department.departmentId, list.listId)">
                     {{ list.name }} ({{ list.numberOfItems }})
                 </li>
             </ul>
         </div>
-        <div v-else>
-            No lists to work on
+        <div class="drafts">
+            <em>Draft Lists</em>
+            <li style="list-style: none; margin-left:10px;">Nothing to see here....</li>
         </div>
         <div>
-            <li class="add-list-button" @click="addListModal">Add List</li>
+            <!-- <li class="add-list-button" @click="addListModal">Create New List</li> -->
+            <button class="button is-secondary is-small"  style="margin-top:20px; margin-bottom: 20px;">Create New List</button>
         </div>
-        <ul>
-            <router-link to="completedlists">
-                <li class="completed">View Completed Lists</li>
-            </router-link>
-        </ul>
-        <ul>
-            <router-link to="completedlists">
-                <li class="completed">View Item Catalog</li>
-            </router-link>
-        </ul>
-        <span class="sidebar-header"><span class="icon"><i class="fa fa-filter"></i></span>Filters</span>
-         <ul>
-            <li :class="{ 'active': selectedOption === 'all' }"
-                @click="navigateTo(this.$store.state.activeList.departmentId, this.$store.state.activeList.listId, 'all')">
-                All
-                Items</li>
-            <li :class="{ 'active': selectedOption === 'unassigned' }" @click="filterByUnassigned()">Unassigned Items</li>
-            <li :class="{ 'active': selectedOption === 'claimed' }" @click="filterByClaimed()">My Claimed Items</li>
-        </ul>
+
+        <div class="admin">
+            <em>ADMIN</em>
+            <ul>
+                <router-link to="testing">
+                    <li class="completed">View Completed Lists</li>
+                </router-link>
+            </ul>
+            <ul>
+                <router-link to="testing">
+                    <li class="completed">View Item Catalog</li>
+                </router-link>
+            </ul>
+            <ul>
+                <router-link to="testing">
+                    <li class="completed">View Purchased Items</li>
+                </router-link>
+            </ul>
+        </div>
+
+
+        
+
         <div>&nbsp;</div>
     </div>
 </template>
-    
+  
 <script>
 import ShoppingListService from '../services/ShoppingListService';
+import DepartmentService from '../services/DepartmentService';
 
 export default {
     data() {
         return {
-            lists: [
-            ],
-            invitedLists: [
-            ],
+            lists: [],
+            invitedLists: [],
             selectedOption: null,
+            departments: [],
         };
     },
 
     computed: {
-        departments() {
-            const departments = new Set();
-            this.invitedLists.forEach(dept => departments.add(dept.departmentName));
-            return Array.from(departments).sort((a, b) => (a.departmentName > b.departmentName) ? 1 : -1);
-        },
         inProgressLists() {
-            return this.lists.filter(list => list.status == 2);
+            return this.lists.filter((list) => list.status == 2);
         },
     },
 
     created() {
+        DepartmentService.getDepartments()
+            .then((response) => {
+                this.departments = response.data;
+            })
+            .catch((error) => {
+                console.error('Error fetching departments:', error);
+            });
         ShoppingListService.getListsInProgress(this.$store.state.user.departmentId)
-            .then(response => {
-                // API response data will need to contain array of shopping lists
-                this.lists = response.data.sort((a, b) => (a.name > b.name) ? 1 : -1);
+            .then((response) => {
+                this.lists = response.data.sort((a, b) => (a.name > b.name ? 1 : -1));
             })
-            .catch(error => {
+            .catch((error) => {
                 console.error('Error fetching lists:', error);
-            })
+            });
 
         ShoppingListService.getInvitedListsInProgress(this.$store.state.user)
-            .then(response => {
-                this.invitedLists = response.data.sort((a, b) => (a.name > b.name) ? 1 : -1);
+            .then((response) => {
+                this.invitedLists = response.data.sort((a, b) => (a.name > b.name ? 1 : -1));
             })
-            .catch(error => {
+            .catch((error) => {
                 console.error('Error fetching invited lists:', error);
-            })
-
+            });
     },
 
     methods: {
         navigateTo(departmentId, listId) {
             let activeList = null;
             ShoppingListService.getSpecificList(departmentId, listId)
-                .then(response => {
+                .then((response) => {
                     this.$store.commit('SET_ITEMS', response.data);
                     if (this.lists.find((element) => element.listId == listId)) {
-                        activeList = this.lists.find((element) => element.listId == listId)
+                        activeList = this.lists.find((element) => element.listId == listId);
                     } else if (this.invitedLists.find((element) => element.listId == listId)) {
-                        activeList = this.invitedLists.find((element) => element.listId == listId)
+                        activeList = this.invitedLists.find((element) => element.listId == listId);
                     }
-                    this.$store.commit('SET_ACTIVE_LIST', activeList)
-
+                    this.$store.commit('SET_ACTIVE_LIST', activeList);
                 })
-                .catch(error => {
+                .catch((error) => {
                     console.error('Error fetching list:', error);
                 });
             this.selectedOption = 'all';
         },
         filterByClaimed() {
             ShoppingListService.getListFilteredByClaimed(this.$store.state.activeList.listId, this.$store.state.user.userId, this.$store.state.activeList.departmentId)
-                .then(response => {
+                .then((response) => {
                     this.$store.commit('SET_ITEMS', response.data);
                 })
-                .catch(error => {
+                .catch((error) => {
                     console.error('Error filtering list:', error);
-                })
+                });
             this.selectedOption = 'claimed';
         },
         filterByUnassigned() {
             ShoppingListService.getListFilteredByUnassigned(this.$store.state.activeList.listId, this.$store.state.activeList.departmentId)
-                .then(response => {
+                .then((response) => {
                     this.$store.commit('SET_ITEMS', response.data);
                 })
-                .catch(error => {
+                .catch((error) => {
                     console.error('Error filtering list:', error);
-                })
+                });
             this.selectedOption = 'unassigned';
         },
         inProgressListsByDepartment(department) {
-            return this.invitedLists.filter(list => list.departmentName === department && list.status == 2);
+            return this.lists.filter((list) => list.departmentName === department && list.status == 2);
         },
-    }
-}
+    },
+};
 </script>
     
 <style scoped>
@@ -158,6 +164,7 @@ li {
     cursor: pointer;
     padding: 5px;
     border-radius: 5px;
+    font-size: small;
 }
 
 li:hover {
@@ -177,11 +184,13 @@ li:hover {
     font-weight: bold;
     padding: 15px 0px 0px 0px;
     display: block;
+    margin-bottom: 5px;
 }
 
 .completed {
     color: #fff
 }
+
 .add-list-button {
     list-style: none;
     font-style: italic;
@@ -190,5 +199,14 @@ li:hover {
 
 }
 
+.drafts {
+    margin-top: 30px;
+}
+
+em {
+    font-size: medium;
+    font-weight: bold;
+    font-style: normal;
+}
 </style>
     
