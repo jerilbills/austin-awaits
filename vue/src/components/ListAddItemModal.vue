93ee6e057a2b3fcd214baa39ae916ee81591eb12
joshfutrell <futrell.joshua@gmail.com>
2023-12-13 11:29:15 -0500

    Styled Add List Item modal fields

--- vue/src/components/ListAddItemModal.vue
+++ vue/src/components/ListAddItemModal.vue
@@ -4,19 +4,38 @@
     <div class="modal-content" style="background-color: #ffffff; max-width: 500px; padding: 20px;">
       <button class="delete is-large" aria-label="close" @click="closeModal"
         style="position: absolute; top: 10px; right: 10px;"></button>
-      <div class="field">
-        <label class="item-name">Item Name</label>
-        <select class="select" v-model="selectedItemId" required>
-          <option v-for="item in availableItems" :key="item.itemId" :value="item.itemId">
-            {{ item.name }}
-          </option>
-        </select>
-      </div>
-      <div class="field">
-        <label class="quantity">Quantity Needed</label>
-        <input class="input" type="number" v-model="itemQuantity" required>
-      </div>
-      <button class="button is-primary" @click="addItem" style="margin-right: 5px;">Save Item</button>
+
+      <div class="field is-horizontal" style="padding-top: 40px;">
+          <div class="field-label is-normal">
+            <label for="item" class="label">Item&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
+          </div>
+          <div class="field-body">
+            <div class="field">
+              <div class="control is-expanded">
+                <div class="select">
+                  <select id="item" name="item" v-model="selectedItemId" required>
+                    <option v-for="item in availableItems" :key="item.itemId" :value="item.itemId">
+                      {{ item.name }}
+                    </option>
+                  </select>                
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      <div class="field is-horizontal">
+          <div class="field-label is-normal">
+            <label for="quantity" class="label">Quantity</label>
+          </div>
+          <div class="field-body">
+            <div class="field">
+              <p class="control is-expanded">
+                <input id="quantity" name="quantity" class="input" type="number" v-model="itemQuantity" required min="1" :disabled="!selectedItemId">
+              </p>
+            </div>
+          </div>
+        </div>
+      <button class="button is-primary" @click="addItem" style="margin-right: 5px;" :disabled="!hasRequiredFields">Save Item</button>
       <button class="button is-link" @click="closeModal">Cancel</button>
     </div>
   </div>
@@ -31,18 +50,23 @@ export default {
   },
   data() {
     return {
-      selectedItem: null,
-      itemQuantity: "",
+      selectedItemId: null,
+      itemQuantity: 1,
     };
+  },
+  computed: {
+    hasRequiredFields() {
+      return this.selectedItemId && this.itemQuantity > 0;
+    }
   },
   methods: {
     addItem() {
-      if (!this.selectedItemId) {
+      if (!this.selectedItemId && !this.itemQuantity > 0) {
         return;
       }
-      const selectedItem = this.availableItems.find(item => item.itemId === this.selectedItemId);
+      // const selectedItem = this.availableItems.find(item => item.itemId === this.selectedItemId);
       const newItem = {
-        name: selectedItem.name,
+        itemId: this.selectedItemId,
         quantity: this.itemQuantity,
       };
       this.$emit("item-added", newItem);
@@ -53,8 +77,5 @@ export default {
 </script>
 
 <style scoped>
-.item-name {
-  display: flex;
-  flex-direction: column;
-}
+
 </style>
